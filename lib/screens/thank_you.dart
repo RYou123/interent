1e6be4c1c90a1fc6bece7d85b52040aac0896df4
Raif <raifataya@outlook.com>
2022-04-11 18:07:25 +0300

    first commit

--- lib/screens/thank_you.dart
+++ lib/screens/thank_you.dart
@@ -1,8 +1,14 @@
-import '../utils/utils.dart';
+import 'dart:ui';
+
+import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/material.dart';
-import '../widgets/specific_card.dart';
+import 'package:window_location_href/window_location_href.dart';
+
+final String? url = getHref();
+Uri uri = new Uri.dataFromString(url!);
 
 class BookingInfo extends StatelessWidget {
+  String? myurl;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,7 +16,26 @@ class BookingInfo extends StatelessWidget {
           automaticallyImplyLeading: false,
           elevation: 0,
           actions: [],
+          leading: Transform.scale(
+              scale: 3,
+              child: IconButton(
+                onPressed: _launchURL,
+                icon: Image.asset('assets/logo.jpg'),
+                padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+              )),
         ),
-        body: SizedBox());
+        body: ElevatedButton(
+          child: Text("Return"),
+          onPressed: _launchURL,
+        ));
+  }
+}
+
+_launchURL() async {
+  const url = 'https://interent.gr/';
+  if (await canLaunch(url)) {
+    await launch(url, webOnlyWindowName: '_self');
+  } else {
+    throw 'Could not launch $url';
   }
 }
